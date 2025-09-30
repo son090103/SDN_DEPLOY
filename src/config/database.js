e30106eac863729e77d4101367e7a1a68f50dea6
son090103 <daoson090103@gmail.com>
2025-09-30 02:51:07 +0200

    updaate

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 module.exports.connect = () => {
   try {
     mongoose
-      .connect("mongodb://127.0.0.1:27017/hotel_db")
+      .connect(process.env.MONGODB_URL)
       .then(() => console.log("Connected!")); // đã kết nối thành công
   } catch (error) {
     console.log("error:", error);
