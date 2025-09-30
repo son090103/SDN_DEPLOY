2eb93556fbeab953427521f722ae56b53c3c4503
son090103 <daoson090103@gmail.com>
2025-09-30 02:55:48 +0200

    updaate

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 const database = require("./src/config/database");
 const routerClient = require("./src/routers/index.routes");
 app.use(express.json());
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 module.exports.connect = () => {
   try {
-    mongoose
-      .connect(process.env.MONGODB_URL)
-      .then(() => console.log("Connected!")); // đã kết nối thành công
+    mongoose.connect(process.env.MONGODB_URL).then(() => {
+      console.log("Connected!");
+    }); // đã kết nối thành công
   } catch (error) {
     console.log("error:", error);
   }
